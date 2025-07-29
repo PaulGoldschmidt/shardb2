1a35fbd24a93d6a38a7013edbde2c9ef64f37ac8
Paul Goldschmidt <mail@paul-goldschmidt.de>
2025-07-29 15:32:13 -0700

    only ios 18

--- shardb2/Package.swift
+++ shardb2/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "shardb2",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v18)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
